7fcf5baebfee5a4c5fcddc70920704f7b40b67d9
Allename <allename.dev@gmail.com>
2026-09-16 02:19:05 +0100

    assets: add icon set for documents, key dates, dashboard, and stage reorder
    New icons for the documents redesign (file-type badges: pdf/png/mov/xls/txt/
    mp3, category colors: blue/pink/amber/purple/green document), the key dates
    date-picker suffix icon, the dashboard empty-project folder state, the stage
    drag handle, and a secondary edit glyph used in task/update menus.

--- packages/frontend/src/assets/icons2/icon2.ts
+++ packages/frontend/src/assets/icons2/icon2.ts
@@ -1,49 +1,72 @@
-import FolderIcon from '@/assets/icons2/folder.icon.svg'
-import ListIcon from '@/assets/icons2/list.icon.svg'
-import GridIcon from '@/assets/icons2/grid.icon.svg'
-import ConstructionIcon from '@/assets/icons2/construction.icon.svg'
-import PreConstructionIcon from '@/assets/icons2/pre-construction.icon.svg'
-import MyTeamIcon from '@/assets/icons2/my-team.icon.svg'
-import FolderAddIcon from '@/assets/icons2/folder-add.icon.svg'
-import FolderImportIcon from '@/assets/icons2/folder-import.icon.svg'
-import PlusIcon from '@/assets/icons2/plus.icon.svg'
-import HouseIcon from '@/assets/icons2/house.icon.svg'
-import FactoryIcon from '@/assets/icons2/factory.icon.svg'
-import SkyscraperIcon from '@/assets/icons2/skyscraper.icon.svg'
-import SpannerIcon from '@/assets/icons2/spanner.icon.svg'
-import CraneIcon from '@/assets/icons2/crane.icon.svg';
-import FolderBlackIcon from '@/assets/icons2/folder-black.icon.svg';
-import MoneyBagIcon from '@/assets/icons2/money-bag.icon.svg';
-import BuilderIcon from '@/assets/icons2/builder.icon.svg';
-import GlobeIcon from '@/assets/icons2/globe.icon.svg';
-import AttentionIcon from '@/assets/icons2/attention.icon.svg';
-import NotificationIcon from '@/assets/icons2/notification.icon.svg';
-import SearchIcon from '@/assets/icons2/search.icon.svg';
-import LogoutIcon from '@/assets/icons2/logout.icon.svg';
-import SettingsIcon from '@/assets/icons2/settings.icon.svg';
-import CheckIcon from '@/assets/icons2/check.icon.svg';
-import AgreementIcon from '@/assets/icons2/agreement.icon.svg';
-import PrivacyIcon from '@/assets/icons2/privacy.icon.svg';
-import AddUserIcon from '@/assets/icons2/add-user.icon.svg';
-import PersonIcon from '@/assets/icons2/person.icon.svg';
-import CityIcon from '@/assets/icons2/city.icon.svg';
-import OverviewIcon from '@/assets/icons2/overview.icon.svg';
-import BulbIcon from '@/assets/icons2/bulb.icon.svg';
-import TrendUpIcon from '@/assets/icons2/trend-up.icon.svg';
-import RefreshIcon from '@/assets/icons2/refresh.icon.svg';
-import MoneyIcon from '@/assets/icons2/money.icon.svg';
-import StarsIcon from '@/assets/icons2/stars.icon.svg';
-import TeamIcon from '@/assets/icons2/team.icon.svg';
-import MessagesIcon from '@/assets/icons2/messages.icon.svg';
-import BoldIcon from '@/assets/icons2/bold.icon.svg';
-import ItalicIcon from '@/assets/icons2/italic.icon.svg';
-import UnorderedListIcon from '@/assets/icons2/unordered-list.icon.svg';
-import OrderedListIcon from '@/assets/icons2/ordered-list.icon.svg';
-import ImageIcon from '@/assets/icons2/image.icon.svg';
-import StrikethroughIcon from '@/assets/icons2/strikethrough.icon.svg';
-import CommentIcon from '@/assets/icons2/comment.icon.svg';
-import EditIcon from '@/assets/icons2/edit.icon.svg';
-import DeleteIcon from '@/assets/icons2/delete.icon.svg';
+import FolderIcon from "@/assets/icons2/folder.icon.svg";
+import ListIcon from "@/assets/icons2/list.icon.svg";
+import GridIcon from "@/assets/icons2/grid.icon.svg";
+import ConstructionIcon from "@/assets/icons2/construction.icon.svg";
+import PreConstructionIcon from "@/assets/icons2/pre-construction.icon.svg";
+import MyTeamIcon from "@/assets/icons2/my-team.icon.svg";
+import FolderAddIcon from "@/assets/icons2/folder-add.icon.svg";
+import FolderImportIcon from "@/assets/icons2/folder-import.icon.svg";
+import PlusIcon from "@/assets/icons2/plus.icon.svg";
+import HouseIcon from "@/assets/icons2/house.icon.svg";
+import FactoryIcon from "@/assets/icons2/factory.icon.svg";
+import SkyscraperIcon from "@/assets/icons2/skyscraper.icon.svg";
+import SpannerIcon from "@/assets/icons2/spanner.icon.svg";
+import CraneIcon from "@/assets/icons2/crane.icon.svg";
+import FolderBlackIcon from "@/assets/icons2/folder-black.icon.svg";
+import MoneyBagIcon from "@/assets/icons2/money-bag.icon.svg";
+import BuilderIcon from "@/assets/icons2/builder.icon.svg";
+import GlobeIcon from "@/assets/icons2/globe.icon.svg";
+import AttentionIcon from "@/assets/icons2/attention.icon.svg";
+import NotificationIcon from "@/assets/icons2/notification.icon.svg";
+import SearchIcon from "@/assets/icons2/search.icon.svg";
+import LogoutIcon from "@/assets/icons2/logout.icon.svg";
+import SettingsIcon from "@/assets/icons2/settings.icon.svg";
+import CheckIcon from "@/assets/icons2/check.icon.svg";
+import AgreementIcon from "@/assets/icons2/agreement.icon.svg";
+import PrivacyIcon from "@/assets/icons2/privacy.icon.svg";
+import AddUserIcon from "@/assets/icons2/add-user.icon.svg";
+import PersonIcon from "@/assets/icons2/person.icon.svg";
+import CityIcon from "@/assets/icons2/city.icon.svg";
+import OverviewIcon from "@/assets/icons2/overview.icon.svg";
+import BulbIcon from "@/assets/icons2/bulb.icon.svg";
+import TrendUpIcon from "@/assets/icons2/trend-up.icon.svg";
+import RefreshIcon from "@/assets/icons2/refresh.icon.svg";
+import MoneyIcon from "@/assets/icons2/money.icon.svg";
+import StarsIcon from "@/assets/icons2/stars.icon.svg";
+import TeamIcon from "@/assets/icons2/team.icon.svg";
+import MessagesIcon from "@/assets/icons2/messages.icon.svg";
+import BoldIcon from "@/assets/icons2/bold.icon.svg";
+import ItalicIcon from "@/assets/icons2/italic.icon.svg";
+import UnorderedListIcon from "@/assets/icons2/unordered-list.icon.svg";
+import OrderedListIcon from "@/assets/icons2/ordered-list.icon.svg";
+import ImageIcon from "@/assets/icons2/image.icon.svg";
+import StrikethroughIcon from "@/assets/icons2/strikethrough.icon.svg";
+import CommentIcon from "@/assets/icons2/comment.icon.svg";
+import EditIcon from "@/assets/icons2/edit.icon.svg";
+import DeleteIcon from "@/assets/icons2/delete.icon.svg";
+import FolderGreyIcon from "@/assets/icons2/folder-grey.icon.svg";
+import CalendarIcon from "@/assets/icons2/calendar.icon.svg";
+import ReorderIcon from "@/assets/icons2/reorder.icon.svg";
+import PDFIcon from "@/assets/icons2/pdf.icon.svg";
+import PNGIcon from "@/assets/icons2/png.icon.svg";
+import MOVIcon from "@/assets/icons2/mov.icon.svg";
+import XLSXIcon from "@/assets/icons2/xls.icon.svg";
+import TXTIcon from "@/assets/icons2/txt.icon.svg";
+import MP3Icon from "@/assets/icons2/mp3.icon.svg";
+import BlueDocIcon from "@/assets/icons2/blue-document.icon.svg";
+import PinkDocIcon from "@/assets/icons2/pink-document.icon.svg";
+import AmberDocIcon from "@/assets/icons2/amber-document.icon.svg";
+import PurpleDocIcon from "@/assets/icons2/purple-document.icon.svg";
+import GreenDocIcon from "@/assets/icons2/green-document.icon.svg";
+import EditTwoIcon from "@/assets/icons2/edit-2.icon.svg";
+
+export const documentIcons = [
+  BlueDocIcon,
+  PinkDocIcon,
+  AmberDocIcon,
+  PurpleDocIcon,
+  GreenDocIcon,
+] as const;
 
 export const icons2 = {
   folder: FolderIcon,
@@ -51,7 +74,7 @@ export const icons2 = {
   grid: GridIcon,
   construction: ConstructionIcon,
   preConstruction: PreConstructionIcon,
-  myTeam:MyTeamIcon,
+  myTeam: MyTeamIcon,
   folderAdd: FolderAddIcon,
   folderImport: FolderImportIcon,
   plus: PlusIcon,
@@ -75,7 +98,7 @@ export const icons2 = {
   addUser: AddUserIcon,
   person: PersonIcon,
   city: CityIcon,
-  overview:OverviewIcon ,
+  overview: OverviewIcon,
   trendUp: TrendUpIcon,
   refresh: RefreshIcon,
   bulb: BulbIcon,
@@ -91,5 +114,21 @@ export const icons2 = {
   strikethrough: StrikethroughIcon,
   comment: CommentIcon,
   edit: EditIcon,
-  delete:DeleteIcon
-}
+  delete: DeleteIcon,
+  folderGrey: FolderGreyIcon,
+  calendar: CalendarIcon,
+  reorder: ReorderIcon,
+  pdf: PDFIcon,
+  png: PNGIcon,
+  mov: MOVIcon,
+  xls: XLSXIcon,
+  txt: TXTIcon,
+  mp3: MP3Icon,
+  blueDoc: BlueDocIcon,
+  pinkDoc: PinkDocIcon,
+  amberDoc: AmberDocIcon,
+  purpleDoc: PurpleDocIcon,
+  greenDoc: GreenDocIcon,
+  documentIcons,
+  editTwo: EditTwoIcon,
+};
